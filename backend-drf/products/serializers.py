e65e3f6c79be24bdbe6cee8a4889af2a2074a339
spoorjith1 <spoorjith1@gmail.com>
2026-04-07 21:16:45 +0530

    fixed image processing

--- backend-drf/products/serializers.py
+++ backend-drf/products/serializers.py
@@ -3,6 +3,11 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+    
     class Meta:
         model = Product
         fields = "__all__"
+    
+    def get_image(self, obj):
+        return obj.image.url if obj.image else None
