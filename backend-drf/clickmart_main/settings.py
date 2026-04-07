3ee078fc2fe1de3b4abf328b6e379d7ef748a41e
spoorjith1 <spoorjith1@gmail.com>
2026-04-07 18:15:30 +0530

    csrf trusted origins

--- backend-drf/clickmart_main/settings.py
+++ backend-drf/clickmart_main/settings.py
@@ -180,4 +180,4 @@ CORS_ALLOWED_ORIGINS = [
     'https://clickmartsp.shop'
 ]
 
-CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
